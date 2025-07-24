45f6e78d4c93d63e3fa03898f86ffba79f28e4da
khalid <khalidabdullahi3000@gmail.com>
2025-07-24 12:20:43 +0100

    Initial commit

--- fitforce/settings.py
+++ fitforce/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+if os.path.isfile('env.py'):
+    import env
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,11 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#58ui+k0a+v_#2$$9#z=s00d#1k)ae0-o8#*cky#dl(3_@4io'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['fitforce.herokuapp.com']
+DEBUG = True
 
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fitforce.herokuapp.com']
 
 
 # Application definition
@@ -76,10 +78,7 @@ WSGI_APPLICATION = 'fitforce.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
@@ -118,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
